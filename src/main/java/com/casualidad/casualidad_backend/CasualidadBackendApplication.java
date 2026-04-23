08120293ef2bfc1a238e0827195f4d8b4db154f5
Gabriel Infante <gabriel.infante@uptc.edu.co>
2026-04-22 21:26:10 -0500

    Creacion proy y ajustes de datadog

---
+++ src/main/java/com/casualidad/casualidad_backend/CasualidadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.casualidad.casualidad_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasualidadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasualidadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casualidad/casualidad_backend/CasualidadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casualidad.casualidad_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasualidadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
