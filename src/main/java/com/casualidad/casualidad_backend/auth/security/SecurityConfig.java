05f0700ed00d4cdb3164939e449222e7f3440003
Gabriel Infante <gabriel.infante@uptc.edu.co>
2026-05-03 09:47:48 -0500

    Se añade cors

--- src/main/java/com/casualidad/casualidad_backend/auth/security/SecurityConfig.java
+++ src/main/java/com/casualidad/casualidad_backend/auth/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.casualidad.casualidad_backend.auth.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +29,29 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 1. Permitir el origen de tu Angular local
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); 
+        
+        // 2. Permitir los métodos HTTP
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        // 3. Permitir los headers (muy importante para enviar el Token Bearer y el Content-Type JSON)
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
+        
+        // 4. Permitir credenciales (si usas cookies o tokens)
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplicar a todas las rutas
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable) // Desactivar CSRF para APIs REST
             .authorizeHttpRequests(auth -> auth
                 // EXCEPCIONES: Rutas públicas que NO piden token
